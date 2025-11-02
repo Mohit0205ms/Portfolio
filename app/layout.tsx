3b85319f84486a2486d12198f45737b3971f380e
Mohit0205ms <mohitsingh0205ms@gmail.com>
2025-11-02 12:15:43 +0530

    create scroll To Top Button feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import FooterSection from "@/components/FooterSection";
+import ScrollToTopButton from "@/components/ScrollToTopButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Navbar/>
         {children}
         <FooterSection/>
+        <ScrollToTopButton/>
       </body>
     </html>
   );
