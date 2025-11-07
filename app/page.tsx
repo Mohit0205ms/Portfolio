231af8efb513b01d9ec73ab48889d9632a618e89
Mohit0205ms <mohitsingh0205ms@gmail.com>
2025-11-07 13:30:56 +0530

    changed the text color

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ function HomeContent() {
   const { data, loading, error } = usePortfolioData();
 
   if (loading) {
-    return <div className='min-h-screen bg-white flex items-center justify-center'>Loading...</div>;
+    return <div className='min-h-screen bg-white flex items-center justify-center text-black'>Loading...</div>;
   }
 
   if (error) {
